15d1c21e380be54d73b10eb5c48e41e0fa091452
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-24 16:17:21 +0530

    more operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,44 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 
 console.log(typeof stringNumber);
+
+// ***********************************Operations *******************************************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello";
+let str2 = " gaurav";
+
+let str3 = str1 + str2;
+console.log(str3); // hello gaurav
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32 ====>   operation calculation starts from left to right
+
+console.log(3 + ((4 * 5) % 3)); // =====> 3 by bodmas rule
+
+console.log(true); //true
+console.log(+true); //1
+// console.log(true+)  //error
+console.log(+""); //0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
