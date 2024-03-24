9c1750fd6b0a4a2e3036cc35b8ca1d1c6b58db0d
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-24 15:27:52 +0530

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello")
+// alert(3+3) // we are using nodejs , not browser ( in newer version ) So this will give error
+
+console.log(3 + 3);
+
+console.log("Gaurav");
+
+let name = "gaurav";
+let age = 18;
+let isLoggedIN = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
