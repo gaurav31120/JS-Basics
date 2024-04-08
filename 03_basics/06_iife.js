94bf0ee7df8393bfb0e80f3b2a1631d5b9bac890
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-08 08:08:18 +0530

    IIFE

---
+++ 03_basics/06_iife.js
@@ -0,0 +1,28 @@
+// Immediately Invoked Function Expressions (UUFE)
+//1. Pollution from global scope - if something is declared in global scope and I don't want that
+// variable to be called from local scope (child) ----- to remove this IIFE is used
+
+//2. Want to immediately execute
+
+//IIFE Syntax    -----    ()()    ----- (function definition)(function execution)
+
+//Named IIFE
+(function chai() {
+  console.log(`DB CONNECTED`);
+})(); // DB CONNECTED
+
+
+(function aurcode() {
+  console.log(`DB CONNECTED2`);
+})(); // DB CONNECTED2
+
+
+// Unnamed IIFE
+(() => {
+  console.log(`DB CONNECTED3`);
+})(); // DB CONNECTED3
+
+
+((name) => {
+  console.log(`DB CONNECTED4 ${name}`);
+})("gaurav"); // DB CONNECTED4 gaurav
