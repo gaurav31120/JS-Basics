4e776b30a0bdeca2cdfe58cd1634e3809fe8f2f0
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-21 19:41:10 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,48 @@
+//switch
+
+//Syntax:
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+
+  default:
+    console.log("April");
+    break;
+}
+
+const month1 = "March";
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+
+  default:
+    console.log("April");
+    break;
+}
