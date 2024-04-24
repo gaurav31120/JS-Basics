b14fa39740b14d736b86d547c73bd51595b54332
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-24 08:13:41 +0530

    for of loop

---
+++ 05_iterations/highOrderArrayLoopsForOfLoop01.js
@@ -0,0 +1,78 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// Syntax:
+//  for (const iterator of object) {
+
+//  }
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+  console.log(num);
+}
+// 1
+// 2
+// 3
+// 4
+// 5
+
+//--------------------------------------------------------------------------------
+const greetings = "Hello world!";
+for (const greet of greetings) {
+  console.log(`Each char is ${greet}`);
+}
+// Each char is H
+// Each char is e
+// Each char is l
+// Each char is l
+// Each char is o
+// Each char is
+// Each char is w
+// Each char is o
+// Each char is r
+// Each char is l
+// Each char is d
+// Each char is !
+
+// ===========================================================================
+
+// Maps ---- stored in key-value pairs -
+//  always have unique values
+//  no duplicate values
+//  maintain order
+
+const map = new Map();
+map.set("IN", "India");
+map.set("USA", "United States of America");
+map.set("FR", "France");
+
+console.log(map);
+// Map(3) {
+//     'IN' => 'India',
+//     'USA' => 'United States of America',
+//     'FR' => 'France'
+//   }
+
+for (const [key, value] of map) {
+  console.log(key, ":-", value);
+}
+
+// IN :- India
+// USA :- United States of America
+// FR :- France
+
+console.log("-------------------------------------------------");
+
+const myObject = {
+  game1: "NFS",
+  game2: "Spiderman",
+};
+
+// for (const [key, value] of myObject) {
+//   console.log(key, ":-", value);           // TypeError: myObject is not iterable
+// }
+
+// --------------------------------------------------
